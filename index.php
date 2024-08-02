87705f58306e8687c664b71861b169472dbc7bd2
Mhmdomarissa <mhmdomarissa1@gmail.com>
2024-08-02 16:21:39 +0300

    variables and datatypes

--- index.php
+++ index.php
@@ -1,11 +1,46 @@
 <?php
 
-    echo" i love pizza<br>";
-    echo "its realy good for the taste";
+    // echo" i love pizza<br>";
+    // echo "its realy good for the taste";
+    $name = "mhmd issa";
+    $food = "pizza";
+    $email = "fake123@gmail.com";
+
+    $age = 22 ;
+    $users = 2 ; 
+    $quantity = 3 ; 
+
+    $gpa = 2.5 ; 
+    $price = 4.99 ; 
+    $taxrate = 0.08 ; 
+
+    $employed = true ; 
+    $online = false ; 
+    $for_sale = true ; 
+
+    // echo "helo {$name} <br>"; 
+    // echo "you like  {$food}";
+    // echo "<br> your email is {$email}";
+    // echo "<br> your age is {$age}";
+    // echo "<br> there are {$users} users in the chatroom";
+    // echo "<br> you would like {$quantity} items ";
+    // echo " your gpa is{$gpa}";
+    // echo "<br> your pizza is \$ {$price}  ";
+    // echo " <br> tax rate is {$taxrate} %  ";
+    // echo "<br> you are {$employed} employed";
+
+    echo "you have ordered {$quantity} and {$food} for sale";
+    $total = $price * $quantity;
+    echo "<br> your total is {$total}"
 
 
-    
-<!-- <!DOCTYPE html>
+
+?>
+
+
+
+<!-- 
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -13,7 +48,7 @@
     <title>Document</title>
 </head>
 <body>
-    <br>
+    <br
     <button>order a pizza </button>
     
 </body>
