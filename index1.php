d1219b23c63f2a1d487bfaaf9526ada559471968
Mhmdomarissa <mhmdomarissa1@gmail.com>
2024-08-02 17:13:06 +0300

    if else statements in php

--- index1.php
+++ index1.php
@@ -6,33 +6,68 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="index1.php" method="post">
+    <!-- <form action="index1.php" method="post">
         <label>quantity : </label><br>
         <input type="text" name="quantity"><br>
         <input type="submit" value="total">
 
 
-    </form>
+    </form> -->
 
     
 </body>
 </html>
 <?php
-$item = "pizza";
-$price = 4.99;
-$quantity = $_POST["quantity"];
-$total = null ;
-$total = $price * $quantity;
+// $item = "pizza";
+// $price = 4.99;
+// $quantity = $_POST["quantity"];
+// $total = null ;
+// $total = $price * $quantity;
 
-echo "you have ordered {$quantity} and {$item} for sale";
-echo "<br> your total is \$ {$total}"
+// echo "you have ordered {$quantity} and {$item} for sale";
+// echo "<br> your total is \$ {$total}"
+
+// $age = -1 ; 
+// if($age >=100) {
+//     echo "you are too old to vote";
+
+// }elseif($age == 0 ){
+//     echo "you are not born yet";
+
+// }elseif($age <=0){
+//     echo "you are under the age of 0";
+
+// }elseif($age >=100) {
+//     echo "you are too old to vote";
+
+// }else { 
+//     echo "you are not eligible to vote";
+// }
+// $adult = true ; 
+// if($adult == true){
+//     echo "you are eligible to vote";
+// }else { 
+//     echo "you are not eligible to vote";
+// }
+
+// $hour = -12 ; 
+// $rate = 10 ; 
+// $weekly_pay = null ; 
 
 
+// IF($hour <= 0 ){
+//     $weekly_pay = 0  ;
 
+// }elseif($hour <= 40){ 
+//     $weekly_pay = $hour * $rate ;
+// }else{
+//     $overtime_hours = $hour - 40 ;
+//     $overtime_rate = 1.5 * $rate ;
+//     $overtime_pay = $overtime_hours * $overtime_rate ;
+//     $weekly_pay = 40 * $rate + $overtime_pay ;
+// }
 
-
-
-
+// echo "your weekly pay is\$ {$weekly_pay}";
 
 
 ?>
