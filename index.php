12615377f78d4a687dba850a3c0f11878d9aaaa5
Mhmdomarissa <mhmdomarissa1@gmail.com>
2024-08-02 16:01:43 +0300

    into to php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+    echo" i love pizza<br>";
+    echo "its realy good for the taste";
+
+
+    
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>order a pizza </button>
+    
+</body>
+</html> -->
+
